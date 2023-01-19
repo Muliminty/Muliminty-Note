3ae2f804bb46fd0120ffd613228d11d236187dd9
DESKTOP-665SHRM\Muliminty <313195303@qq.com>
2023-01-20 00:12:15 +0800

    feat: 更新代码

--- ReadingNotes/javascript设计模式/源代码/p48.js
+++ ReadingNotes/javascript设计模式/源代码/p48.js
@@ -1,1 +1,23 @@
-console.log(11111)
+Function.prototype.before = function (fn) {
+    var _this = this;
+    return function () {
+        fn.apply(this, arguments)
+        return _this.apply(this, arguments)
+    }
+}
+
+Function.prototype.after = function (fn) {
+    var _this = this;
+    return function () {
+        var ret = _this.apply(this, arguments)
+        fn.apply(this, arguments)
+        return ret
+    }
+}
+var func = function () {
+    console.log(2)
+}
+
+func = func.before(() => { console.log(1) }).after(() => { console.log(3) })
+
+func()
